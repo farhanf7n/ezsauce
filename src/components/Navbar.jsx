c63c5fc8e0b56d35b5c64ecdd7cf6b14bf108525
Farhan Ahmed <farhanf7n@gmail.com>
2024-10-10 23:45:26 +0500

    Added dark mode functionality

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,35 +1,39 @@
+import { useState, useEffect } from 'react';
 import siteLogo from '../assets/Logo.svg';
+import sunIcon from '../assets/sun.svg';
+import moonStarsIcon from '../assets/moonStars.svg';
 
 export default function Navbar() {
+  const [isDarkMode, setIsDarkMode] = useState(false);
+
+  useEffect(() => {
+    document.documentElement.classList.toggle('dark', isDarkMode);
+  }, [isDarkMode]);
+
+  const toggleDarkMode = () => {
+    setIsDarkMode(!isDarkMode);
+  };
+
   return (
     <div className="w-[1160px] mx-auto flex items-center justify-between py-[20px]">
       <div>
         <img src={siteLogo} className="w-[32px] h-[32px]" alt="" />
       </div>
       <div className="flex items-center gap-[19px]">
-        <a href="#" className="py-[12px] px-[18px]">
-          <svg
-            className="moonSvg"
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 24 24"
-            width="24"
-            height="24"
-            color="#000000"
-            fill="none"
-          >
-            <path
-              d="M21.5 14.0784C20.3003 14.7189 18.9301 15.0821 17.4751 15.0821C12.7491 15.0821 8.91792 11.2509 8.91792 6.52485C8.91792 5.06986 9.28105 3.69968 9.92163 2.5C5.66765 3.49698 2.5 7.31513 2.5 11.8731C2.5 17.1899 6.8101 21.5 12.1269 21.5C16.6849 21.5 20.503 18.3324 21.5 14.0784Z"
-              stroke="currentColor"
-              strokeWidth="1.5"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-            />
-          </svg>
-        </a>
+        <button
+          onClick={toggleDarkMode}
+          className="p-2 rounded-full"
+        >
+          <img
+            src={isDarkMode ? sunIcon : moonStarsIcon}
+            alt={isDarkMode ? "Switch to Light Mode" : "Switch to Dark Mode"}
+            className="w-6 h-6"
+          />
+        </button>
 
         <a
           href="#"
-          className="py-[12px] px-[18px] flex items-center gap-[8px] border border-1 border-slate-grey rounded-[8px] hover:bg-black hover:text-white hover:border-black shadow-drop"
+          className="py-[12px] px-[18px] flex items-center gap-[8px] border border-1 border-slate-gray rounded-[8px] hover:bg-black hover:text-white hover:border-black shadow-drop"
         >
           About
         </a>
