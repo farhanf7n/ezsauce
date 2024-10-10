21fefb872dae5ef4d343705cdbb845b34db6d8e7
Farhan Ahmed <farhanf7n@gmail.com>
2024-10-10 23:44:12 +0500

    Added dark mode in config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  darkMode: 'class',
   theme: {
     extend: {
       colors: {
-        'slate-grey': '#B1B1B1',
-        'medium-grey': '#828282',
+        'slate-gray': '#B1B1B1',
+        'medium-gray': '#828282',
+        'light-gray': '#efefef'
       },
       fontFamily: {
         ClashDisplayBold: ['ClashDisplay-Bold', 'sans'],
