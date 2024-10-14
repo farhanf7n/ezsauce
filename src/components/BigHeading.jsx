0159542c332f4a4a81ce27c20768266fe0b6f1dd
Farhan Ahmed <farhanf7n@gmail.com>
2024-10-14 23:13:22 +0500

    Changed Heading Color

--- src/components/BigHeading.jsx
+++ src/components/BigHeading.jsx
@@ -1,6 +1,6 @@
 export default function BigHeading() {
   return (
-    <div className="pt-[30px] w-[1160px] mx-auto border-b border-1">
+    <div className="pt-[30px] w-[1160px] mx-auto border-b border-1 border-slate-gray">
       <p className="text-[232px] text-center font-bold font-ClashDisplayBold leading-[228px] text-[#B1B1B1]">
         EZSAUCE
       </p>
