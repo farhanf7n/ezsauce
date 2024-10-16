0ce302398d08dff760c8781c5e05e84164d3223b
Farhan Ahmed <farhanf7n@gmail.com>
2024-10-17 04:24:25 +0500

    Minor fixes for responsiveness

--- src/components/BigHeading.jsx
+++ src/components/BigHeading.jsx
@@ -1,7 +1,7 @@
 export default function BigHeading() {
   return (
     <div className="w-full lg:w-[1160px] px-5 py-7 md:pt-[30px] mx-auto border-b border-1 border-slate-gray">
-      <p className="text-7xl lg:text-[212px] lg:leading-[228px] text-center font-bold font-ClashDisplayBold text-[#B1B1B1] dark:text-[#FFC70F]">
+      <p className="text-7xl sm:text-9xl lg:text-[212px] lg:leading-[228px] text-center font-bold font-ClashDisplayBold text-[#B1B1B1] dark:text-[#FFC70F]">
         EZSAUCE
       </p>
     </div>
--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,28 +1,28 @@
 import guyUsingLaptop from '../../public/assets/guy-using-laptop.svg';
 export default function HeroSection() {
   return (
-    <div className="hero-container w-full lg:w-[1160px] mx-auto flex items-center justify-between relative px-5">
+    <div className="hero-container w-full pt-[90px] lg:w-[1160px] mx-auto flex items-center justify-between relative px-5">
       <div>
-        <p className="hero-heading font-ClashDisplayMedium relative text-4xl lg:text-[98px] leading-[98px] flex flex-col">
+        <p className="hero-heading font-ClashDisplayMedium relative text-4xl lg:text-[98px] leading-none lg:leading-[98px] flex flex-col">
           <span className="font-ClashDisplayMedium">
             The{' '}
-            <span className="relative font-ClashDisplayMedium before:block before:absolute before:bg-[#FFC70F] before:h-[16px] before:w-full before:bottom-[8px] before:right-0 before:z-[-1]">
+            <span className="relative font-ClashDisplayMedium before:block before:absolute before:bg-[#FFC70F] before:h-[10px] lg:before:h-[16px] before:w-full before:bottom-[8px] before:right-0 before:z-[-1]">
               one-stop
             </span>
             shop
           </span>
           <span className="font-ClashDisplayMedium">
             with{' '}
-            <span className="relative text-white font-ClashDisplayMedium before:block before:absolute before:bg-[#000000] before:h-[85px] before:w-full before:bottom-[8px] before:right-0 before:z-[-1]">
+            <span className="relative text-white font-ClashDisplayMedium before:block before:absolute before:bg-[#000000] before:h-[10px] lg:before:h-[85px] before:w-full before:bottom-[8px] before:right-0 before:z-[-1]">
               everything
             </span>
           </span>
 
           <span>
-            <span className="relative font-ClashDisplayMedium before:block before:absolute before:bg-[#FFC70F] before:h-[16px] before:w-full before:bottom-[8px] before:right-0 before:z-[-1]">
+            <span className="relative font-ClashDisplayMedium before:block before:absolute before:bg-[#FFC70F] before:h-[10px] md:before:h-[16px] before:w-full before:bottom-[8px] before:right-0 before:z-[-1]">
               you
             </span>{' '}
-            <span className="text-white relative font-ClashDisplayMedium before:block before:absolute before:bg-[#000000] before:h-[85px] before:w-full before:bottom-[8px] before:right-0 before:z-[-1]">
+            <span className="text-white relative font-ClashDisplayMedium before:block before:absolute before:bg-[#000000] before:h-[10px] lg:before:h-[85px] before:w-full before:bottom-[8px] before:right-0 before:z-[-1]">
               need
             </span>
           </span>
