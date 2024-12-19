d6813e4dc07215ced6a0911df4d482c9468b64d7
Farhan Ahmed <farhanf7n@gmail.com>
2024-12-19 05:55:59 +0500

    Made tags component

---
+++ src/components/Tag.jsx
@@ -0,0 +1,14 @@
+import { twMerge } from 'tailwind-merge';
+
+export default function Tag({ children, className }) {
+  return (
+    <span
+      className={twMerge(
+        'bg-[#FFC70F] text-[12px] font-medium leading-[20px] rounded-[4px] py-[4px] px-[6px] dark:text-black',
+        className,
+      )}
+    >
+      {children}
+    </span>
+  );
+}
